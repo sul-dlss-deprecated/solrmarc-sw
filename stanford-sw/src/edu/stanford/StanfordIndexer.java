d358f57e8d047e489c07d8061b97bcb35633086c
Laney McGlohon <laneymcg@stanford.edu>
2014-05-28 16:07:16 -0700

    remove clearing of building_facet and addition of PHYSTEMP to locations_online_list.properties

--- stanford-sw/src/edu/stanford/StanfordIndexer.java
+++ stanford-sw/src/edu/stanford/StanfordIndexer.java
@@ -1198,7 +1198,7 @@ public class StanfordIndexer extends org.solrmarc.index.SolrIndexer
 					i.remove();
 				} else {
 					element.setisOnline(true);
-					element.setLibrary("");
+//					element.setLibrary("");
 				}
 			}
 		}
