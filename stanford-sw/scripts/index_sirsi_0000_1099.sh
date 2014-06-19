c167ea65ff3e57279520a417a8e74bb0b6a5cdcd
Naomi Dushay <ndushay@stanford.edu>
2014-06-19 14:25:37 -0700

    revise full indexing scripts for additional segment

--- stanford-sw/scripts/index_sirsi_0000_1099.sh
+++ stanford-sw/scripts/index_sirsi_0000_1099.sh
@@ -2,10 +2,10 @@
 # index_all_sirsi.sh
 # Import all marc files from sirsi full dump into a Solr index  (SearchWorks flavor)
 #
-# updated for Naomi's FORK of solrmarc 2011-01-23
-# Naomi Dushay 2013-08-14
 # added index_sirsi_1050_1099.sh
 # Laney McGlohon 2014-06-16
+# updated for Naomi's FORK of solrmarc 2011-01-23
+# Naomi Dushay 2013-08-14
 
 # take an argument for the name of the log subdirectory
 LOG_SUBDIR=$1
@@ -27,8 +27,7 @@ ant -buildfile $SOLRMARC_BASEDIR/build.xml dist_site
 /home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_0700_0799.sh $LOG_SUBDIR &
 /home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_0800_0899.sh $LOG_SUBDIR &
 /home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_0900_0999.sh $LOG_SUBDIR &
-/home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_1000_1049.sh $LOG_SUBDIR &
-/home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_1050_1099.sh $LOG_SUBDIR &
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/index_sirsi_1000_1099.sh $LOG_SUBDIR &
 #curl http://localhost:8983/solr/update?commit=true
 
 echo "!!! SOLR COMMIT  curl http://localhost:8983/solr/update?commit=true !!!"
