085c0a549239b97d5c10aeeb95c8a227b847b268
Laney McGlohon <laneymcg@stanford.edu>
2015-05-25 08:07:53 -0700

    Add sirsi as the first collection in all records

--- stanford-sw/test/src/edu/stanford/ManagedPurlTests.java
+++ stanford-sw/test/src/edu/stanford/ManagedPurlTests.java
@@ -57,7 +57,7 @@ public class ManagedPurlTests extends AbstractStanfordTest
 		assertDocHasFieldValue("managedPurlItem3Collections", fldName, "citation"); 
 		assertDocHasFieldValue("ManagedAnd2UnmanagedPurlCollection", fldName, "image"); 
 		
-		assertDocHasNoField("NoManagedPurlItem", fldName);
+		assertDocHasFieldValue("NoManagedPurlItem", fldName, "sirsi");
 		assertDocHasNoFieldValue("ManagedAnd2UnmanagedPurlCollection", fldName, "book");
 	}
 	
@@ -72,9 +72,9 @@ public class ManagedPurlTests extends AbstractStanfordTest
 		String fldName = "collection";
 			
 		assertDocHasFieldValue("managedPurlItem1Collection", fldName, "yg867hg1375"); 
-		assertDocHasFieldValue("amanagedPurlItem3Collections", fldName, "yg867hg1375, aa000bb111, yy000zz0000"); 
-		assertDocHasNoField("ManagedAnd2UnmanagedPurlCollection", fldName); 
-		assertDocHasNoField("NoManagedPurlItem", fldName); 
+		assertDocHasFieldValue("managedPurlItem3Collections", fldName, "yy000zz1111");
+		assertDocHasFieldValue("ManagedAnd2UnmanagedPurlCollection", fldName, "sirsi");
+		assertDocHasFieldValue("NoManagedPurlItem", fldName, "sirsi");
 	}
 
 	/**
@@ -86,9 +86,8 @@ public class ManagedPurlTests extends AbstractStanfordTest
 	{
 		String fldName = "collection_with_title";
 			
-		assertDocHasFieldValue("managedPurlItem1Collection", fldName, "[yg867hg1375-|-Francis E. Stafford photographs, 1909-1933]"); 
-		assertDocHasFieldValue("amanagedPurlItem3Collections", fldName, "[yg867hg1375-|-Francis E. Stafford photographs, 1909-1933, aa000bb1111-|-Test Collection, 1963-2015, yy000zz1111-|-Test Collection2, 1968-2015]"); 
-
+		assertDocHasFieldValue("managedPurlItem1Collection", fldName, "yg867hg1375-|-Francis E. Stafford photographs, 1909-1933");
+		assertDocHasFieldValue("managedPurlItem3Collections", fldName, "yy000zz1111-|-Test Collection2, 1968-2015");
 		assertDocHasNoField("managedPurlCollection", fldName);
 		assertDocHasNoField("ManagedAnd2UnmanagedPurlCollection", fldName);
 		assertDocHasNoField("NoManagedPurlItem", fldName); 
