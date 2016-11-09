2ec50197c81ad546f43f518ba644aad9ba157b50
Shelley Doljack <sdoljack@stanford.edu>
2016-11-09 11:49:05 -0800

    Adds script to get many nightly files.

---
+++ stanford-sw/scripts/getBodoniNightlyMany.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getBodoniNightly.sh 161105
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getBodoniNightly.sh 161106
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getBodoniNightly.sh 161107
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getBodoniNightly.sh 161108
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getBodoniNightly.sh 161109
