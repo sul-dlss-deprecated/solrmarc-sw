1a6ded353304a19edeb9ad4bccae847822eeadfa
sw-solr-sirsi-test blacklight <blacklight@sw-solr-sirsi-test.stanford.edu>
2016-12-05 13:14:52 -0800

    Adds scripts for indexing from  morison test server.

---
+++ stanford-sw/scripts/getMorisonNightlyMany.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150121
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150122
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150123
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150124
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150125
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150126
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150127
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150128
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150129
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150130
+/home/blacklight/solrmarc-sw/stanford-sw/scripts/getMorisonNightly.sh 150131
+
